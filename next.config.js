f739417badeb7f6bd1ec41aebc4645b316871b36
Rasmus Lövegren <rasmus@lovegren.fi>
2023-05-17 14:10:45 +0300

    renovatebot and small fixes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  // output: 'standalone',
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     // Required:
