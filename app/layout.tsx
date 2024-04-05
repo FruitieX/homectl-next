bcc6d7384663cc028738c868600891eb7b50a961
Rasmus Lövegren <rasmus@lovegren.fi>
2024-04-05 17:03:07 +0300

    feat: add missing hue devices, move providers to own module

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,60 +1,11 @@
-// https://github.com/pmndrs/jotai/issues/1529#issuecomment-1302156604
-'use client';
-
-import { useProvideWebsocketState } from '@/hooks/websocket';
 import '@/styles/globals.css';
-import { BottomNavigation } from '@/ui/BottomNavigation';
-import { ColorPickerModal } from '@/ui/ColorPickerModal';
-import { Navbar } from '@/ui/Navbar';
-import { SaveSceneModal } from '@/ui/SaveSceneModal';
-import { SceneModal } from '@/ui/SceneModal';
-import { useProvideAppConfig } from '@/hooks/appConfig';
-import { useEffect } from 'react';
-
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  useProvideWebsocketState();
-
-  return (
-    <>
-      <ColorPickerModal />
-      <SaveSceneModal />
-      <SceneModal />
-      <Navbar />
-      <main className="flex min-h-0 flex-1 flex-col">{children}</main>
-      <BottomNavigation />
-    </>
-  );
-};
+import { Providers, ProvideAppConfig, Layout } from './providers';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const appConfigLoaded = useProvideAppConfig();
-
-  // Reload app at 4am
-  useEffect(() => {
-    const now = new Date();
-    const reloadAt = new Date(
-      now.getFullYear(),
-      now.getMonth(),
-      now.getDate(),
-      4,
-      0,
-      0,
-    );
-    reloadAt.setDate(reloadAt.getDate() + 1);
-
-    const reloadTimeout = setTimeout(() => {
-      window.location.reload();
-    }, reloadAt.getTime() - now.getTime());
-
-    return () => {
-      clearTimeout(reloadTimeout);
-    };
-  });
-
   return (
     <html>
       <head>
@@ -70,7 +21,11 @@ export default function RootLayout({
         // Disables scrolling on iOS Safari
         style={{ touchAction: 'none' }}
       >
-        {appConfigLoaded && <Layout>{children}</Layout>}
+        <Providers>
+          <ProvideAppConfig>
+            <Layout>{children}</Layout>
+          </ProvideAppConfig>
+        </Providers>
       </body>
     </html>
   );
---
+++ app/providers.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Provider as JotaiProvider } from 'jotai';
+import { useProvideWebsocketState } from '@/hooks/websocket';
+import '@/styles/globals.css';
+import { BottomNavigation } from '@/ui/BottomNavigation';
+import { ColorPickerModal } from '@/ui/ColorPickerModal';
+import { Navbar } from '@/ui/Navbar';
+import { SaveSceneModal } from '@/ui/SaveSceneModal';
+import { SceneModal } from '@/ui/SceneModal';
+import { useProvideAppConfig } from '@/hooks/appConfig';
+import { useEffect } from 'react';
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <JotaiProvider>{children}</JotaiProvider>;
+};
+
+export const ProvideAppConfig = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const appConfigLoaded = useProvideAppConfig();
+  if (!appConfigLoaded) return null;
+
+  return children;
+};
+
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  useProvideWebsocketState();
+
+  // Reload app at 4am
+  useEffect(() => {
+    const now = new Date();
+    const reloadAt = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      4,
+      0,
+      0,
+    );
+    reloadAt.setDate(reloadAt.getDate() + 1);
+
+    const reloadTimeout = setTimeout(() => {
+      window.location.reload();
+    }, reloadAt.getTime() - now.getTime());
+
+    return () => {
+      clearTimeout(reloadTimeout);
+    };
+  });
+
+  return (
+    <>
+      <ColorPickerModal />
+      <SaveSceneModal />
+      <SceneModal />
+      <Navbar />
+      <main className="flex min-h-0 flex-1 flex-col">{children}</main>
+      <BottomNavigation />
+    </>
+  );
+};
