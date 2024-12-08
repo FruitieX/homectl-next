8b9f50ac0975d79b095dddd9a196aa1645232d64
Rasmus Lövegren <rasmus@lovegren.fi>
2024-12-08 19:39:02 +0200

    chore: upgrade packages and apply codemods

--- app/groups/[id]/page.tsx
+++ app/groups/[id]/page.tsx
@@ -1,18 +1,19 @@
 'use client';
+import { use } from 'react';
 
 import { FlattenedGroupConfig } from '@/bindings/FlattenedGroupConfig';
 import { SceneList } from './SceneList';
 import { useWebsocketState } from '@/hooks/websocket';
 
 type Props = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 export default function Page(props: Props) {
+  const params = use(props.params);
   const state = useWebsocketState();
 
-  const group: FlattenedGroupConfig | undefined =
-    state?.groups[props.params.id];
+  const group: FlattenedGroupConfig | undefined = state?.groups[params.id];
   const groupDevices = group?.device_keys;
 
   if (!groupDevices) return null;
