bf4c9a56e79175ffaecd736d438a1274fc1fd750
Rasmus Lövegren <rasmus@lovegren.fi>
2025-04-22 15:23:24 +0300

    chore: use latest nodejs

--- default.nix
+++ default.nix
@@ -3,6 +3,6 @@ pkgs.mkShell {
   # buildInputs is for dependencies you'd need "at run time",
   # were you to to use nix-build not nix-shell and build whatever you were working on
   nativeBuildInputs = [
-    pkgs.nodejs-18_x
+    pkgs.nodejs
   ];
 }
